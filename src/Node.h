d3173696ab0c9688228c1d8937b3d7fe1ce69a59
Guilherme Figueira <g.figueira.2002@gmail.com>
2025-02-28 19:14:44 +0000

    change license

--- src/Node.h
+++ src/Node.h
@@ -1,11 +1,6 @@
 /******************************************************************************
  * Copyright (c) 2025 Guilherme Figueira
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
+ * MIT License - See LICENSE file in the project root for details
  * Contact: g.figueira.2002@gmail.com
  ******************************************************************************/
 
